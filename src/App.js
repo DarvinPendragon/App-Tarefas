41ff32f05d09de82a9f6374c3c4289004bb845a8
DarvinPendragon <tl.cardoso2@gmail.com>
2021-09-13 16:16:10 -0300

    todas as mudanças do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+import TaskDetails from './components/TaskDetails';
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+const App = () => {
+  // let message = 'hello';
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.cypress.io/todos?_limit=10',
+      );
+      setTasks(data);
+    }
+    fetchTasks();
+  }, []);
+
+  function handleTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function handleTaskAddition(taskTitle) {
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+    setTasks(newTasks);
+  }
+
+  function handleTaskRemove(taskId) {
+    const deleteTasks = tasks.filter((task) => task.id != taskId);
+    console.log(taskId);
+    setTasks(deleteTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskRemove={handleTaskRemove}
+              />
+            </>
+          )}
+        />
+        <Route path="/:TaskTitle" exact component={TaskDetails} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
